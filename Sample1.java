03f0d4a407f60ebb1da78cdb1edfab11a3ae6524
4494 <ogasawara@tbt-s.jp>
2019-04-30 20:50:19 +0900

    add new file

---
+++ Sample1.java
@@ -0,0 +1,14 @@
+class Sample1{
+    public static void main(String[] args) {
+
+        String str = "HELLO";
+
+        char ch1 = str.charAt(0);
+        char ch2 = str.charAt(1);
+        int len = str.length();
+
+        System.out.println(str + "‚Ì‚P”Ô–Ú‚Ì•¶Žš‚Í" + ch1 + "‚Å‚·");
+        System.out.println(str + "‚Ì‚Q”Ô–Ú‚Ì•¶Žš‚Í" + ch2 + "‚Å‚·");
+        System.out.println(str + "‚Ì’·‚³‚Í" + len + "‚Å‚·");
+    }
+}
